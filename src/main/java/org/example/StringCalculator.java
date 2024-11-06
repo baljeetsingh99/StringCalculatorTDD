9a1b77d30d0b3581b023fbd2b6a21a716c48ef51
baljeet singh <prince.singh4000@gmail.com>
2024-11-07 00:01:02 +0530

    Handling different delimiters

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,16 +1,30 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     public int add(String number){
         if(number.isEmpty()){
             return 0;
         }
-        String[] numArr = number.split(",|\n");
+        String defaultDeli = ",|\n";
+        if (number.startsWith("//")) {
+            int delimiterIndex = number.indexOf("\n");
+            defaultDeli = Pattern.quote(number.substring(2, delimiterIndex));
+            number = number.substring(delimiterIndex + 1);
+        }
+        String[] numArr = number.split(defaultDeli);
         int sum = 0;
         for (String n : numArr) {
-            sum += Integer.parseInt(n);
+            if(!n.isEmpty()){
+                int num = Integer.parseInt(n);
+                sum += num;
+            }
+
         }
+
         return sum;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -8,12 +8,16 @@ public class StringCalculatorTest {
 
     @Test
     public void addEmptyStringTest(){
-        assertEquals(0, strCal.add(""));
-        assertEquals(1, strCal.add("1"));
-        assertEquals(9, strCal.add("9"));
-        assertEquals(14,strCal.add("5,9"));
+        assertEquals(0, strCal.add("")); // for empty string
+        assertEquals(1, strCal.add("1")); // for single value
+        assertEquals(9, strCal.add("9")); //for single value
+        assertEquals(14,strCal.add("5,9")); // handling two comma values
         assertEquals(9, strCal.add("2,7"));
-        assertEquals(13, strCal.add("1\n5,7"));
+        assertEquals(10, strCal.add("1,2,3,4")); //handling any amount of numbers
+        assertEquals(13, strCal.add("1\n5,7")); //handling new lines
         assertEquals(16, strCal.add("1\n4\n7,4"));
+        assertEquals(3, strCal.add("//;\n1;2")); //handling different delimeter
+        assertEquals(8, strCal.add("//|\n2|3|3"));
+        assertEquals(3, strCal.add("//;\n1;2"));
     }
 }
